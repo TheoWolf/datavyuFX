30b4572cef52b387d59783cc1be05c4ea3b8471e
Reda Nezzar <nezzar.reda@gmail.com>
2018-04-22 15:18:35 -0400

    DVD-247: First DVMedia commit

--- videoController/src/main/java/media/DVMedia.java
+++ videoController/src/main/java/media/DVMedia.java
@@ -1,5 +1,15 @@
 package media;
 
+import util.Identifier;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum DVMedia {
     INSTANCE;
+
+    private Map<Identifier, DVMedia> medias = new HashMap<>();
+
+    DVMedia(){}
+
 }
