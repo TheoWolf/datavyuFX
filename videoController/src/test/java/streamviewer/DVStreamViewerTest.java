ce35f2159d8f7fac4b50723a94fbd69794b27a05
Reda Nezzar <nezzar.reda@gmail.com>
2018-04-22 13:49:31 -0400

    DVD-254: Added Remove Stream and Updated Add Stream Unit Tests

--- videoController/src/test/java/streamviewer/DVStreamViewerTest.java
+++ videoController/src/test/java/streamviewer/DVStreamViewerTest.java
@@ -10,54 +10,70 @@ import static org.junit.Assert.*;
 
 public class DVStreamViewerTest {
 
-    @Test
-    public void getMedia() {
-        assertTrue(DVStreamViewer.INSTANCE.getMedia() instanceof DVMedia);
+  @Test
+  public void getMedia() {
+    assertTrue(DVStreamViewer.INSTANCE.getMedia() instanceof DVMedia);
+  }
+
+  @Test
+  public void getIdentifier() {
+    assertEquals(DVStreamViewer.INSTANCE.getIdentifier().asLong(), 0L);
+  }
+
+  @Test
+  public void getMasterCurrentTime() {
+
+  }
+
+  @Test
+  public void play() {
+  }
+
+  @Test
+  public void pause() {
+  }
+
+  @Test
+  public void addStream() {
+    //There is always one stream in the streams HashMap, the DVStreamViewer is added
+    //to the streams Map when created, and we have to consider it when checking the size of
+    //streams
+    int streamNumber = 3;
+
+    for(int i = 0; i < streamNumber; i++) {
+      StreamViewer stream = JfxStreamViewer.createStreamViewer(Identifier.generateIdentifier());
+      DVStreamViewer.INSTANCE.addStream(stream);
     }
+    assertEquals(4, DVStreamViewer.INSTANCE.getStreams().size());
+  }
 
-    @Test
-    public void getIdentifier() {
-        assertEquals(DVStreamViewer.INSTANCE.getIdentifier().asLong(), 0L);
-    }
-
-    @Test
-    public void getMasterCurrentTime() {
-
-    }
-
-    @Test
-    public void play() {
-    }
-
-    @Test
-    public void pause() {
-    }
-
-    @Test
-    public void addStream(){
-        int streamNumber = 3;
-
-        for(int i = 0; i < streamNumber; i++){
-            StreamViewer stream = JfxStreamViewer.createStreamViewer(Identifier.generateIdentifier());
-            DVStreamViewer.INSTANCE.addStream(stream);
-        }
-
-        assertEquals(3,DVStreamViewer.INSTANCE.getStreams().size());
-    }
+  @Test
+  public void removeStream() {
+    //There is always one stream in the streams HashMap, the DVStreamViewer is added
+    //to the streams Map when created, and we have to consider it when checking the size of
+    //streams
+    StreamViewer stream1 = JfxStreamViewer.createStreamViewer(Identifier.generateIdentifier());
+    StreamViewer stream2 = JfxStreamViewer.createStreamViewer(Identifier.generateIdentifier());
+    DVStreamViewer.INSTANCE.addStream(stream1);
+    DVStreamViewer.INSTANCE.addStream(stream2);
+    assertEquals(3, DVStreamViewer.INSTANCE.getStreams().size());
+    DVStreamViewer.INSTANCE.removeStream(stream1);
+    assertEquals(2, DVStreamViewer.INSTANCE.getStreams().size());
+  }
 
 
-    @Test
-    public void stop() {
-        //Stop all StreamViewer
-    }
+  @Test
+  public void stop() {
+    //Stop all StreamViewer
+  }
 
-    @Test
-    public void shuttleForward() {
-        //Shuttle Forward all StreamViewer
-    }
+  @Test
+  public void shuttleForward() {
+    //Shuttle Forward all StreamViewer
+  }
 
-    @Test
-    public void shuttleBackward() {
-        //Shuttle Backward all StreamViewer
-    }
+  @Test
+  public void shuttleBackward() {
+    //Shuttle Backward all StreamViewer
+  }
 }
