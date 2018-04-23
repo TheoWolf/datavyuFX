30a25d77d4fa6db0242c7a5bf80db525d8f9d29c
Reda Nezzar <nezzar.reda@gmail.com>
2018-04-23 19:53:54 -0400

    DVD-259: Added a library loader for ffmpeg and avfoundation libraries

--- videoController/src/main/java/util/NativeLibraryLoader.java
+++ videoController/src/main/java/util/NativeLibraryLoader.java
@@ -12,10 +12,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.datavyu.util;
+package util;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -38,15 +36,14 @@ import java.util.Enumeration;
 public class NativeLibraryLoader {
 
     /** Logger for this native library loader */
-    private static Logger logger = LogManager.getLogger(NativeLibraryLoader.class);
+//    private static Logger logger = LogManager.getLogger(NativeLibraryLoader.class);
 
+    public static final int BUFFER_COPY_SIZE = 16*1024;
     /**
      * Folder where we unzip libraries must be current working directory for the library loader to find dependent
      * libraries.
      */
-    private static File libraryFolder = new File(System.getProperty("user.dir"));
-
-    private static boolean isMacOs = System.getProperty("os.name").contains("Mac");
+    private static File libraryFolder = new File(PlatformUtils.USER_DIR);
 
     /**
      * Get resource URL for a given library name that is part of the jar.
@@ -59,7 +56,7 @@ public class NativeLibraryLoader {
         Enumeration<URL> resources;
         String extension;
         ClassLoader classLoader = NativeLibraryLoader.class.getClassLoader();
-        if (isMacOs) {
+        if (PlatformUtils.IS_OS_MAC) {
             extension = ".jnilib";
             resources = classLoader.getResources("lib" + libName + extension);
             if (!resources.hasMoreElements()) {
@@ -82,7 +79,7 @@ public class NativeLibraryLoader {
      */
     private static String getExtension(String libName) throws Exception {
         String extension;
-        if (isMacOs) {
+        if (PlatformUtils.IS_OS_MAC) {
             extension = ".jnilib";
             if (!NativeLibraryLoader.class.getClassLoader()
                     .getResources("lib" + libName + extension).hasMoreElements()) {
@@ -117,15 +114,15 @@ public class NativeLibraryLoader {
      * @throws Exception When extracting or loading the library.
      */
     public static File extract(final String destName) throws Exception {
-        logger.info("Attempting to extract " + destName);
+//        logger.info("Attempting to extract " + destName);
         URL url = getResource(destName);
         InputStream in = url.openStream();
         File outfile = new File(libraryFolder, destName + getExtension(destName));
         FileOutputStream out = new FileOutputStream(outfile);
-        BufferedOutputStream dest = new BufferedOutputStream(out, Constants.BUFFER_COPY_SIZE);
+        BufferedOutputStream dest = new BufferedOutputStream(out, BUFFER_COPY_SIZE);
         int count;
-        byte[] data = new byte[Constants.BUFFER_COPY_SIZE];
-        while ((count = in.read(data, 0, Constants.BUFFER_COPY_SIZE)) != -1) {
+        byte[] data = new byte[BUFFER_COPY_SIZE];
+        while ((count = in.read(data, 0, BUFFER_COPY_SIZE)) != -1) {
             dest.write(data, 0, count);
         }
         dest.close();  // close flushes
